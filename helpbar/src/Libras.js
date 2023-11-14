8b6ab0f0935d1919b4c7ffc26ab7df1c196847aa
MIKAELLA CARDIAL DOS SANTOS <00001085105507sp@aluno.educacao.sp.gov.br>
2023-11-14 15:18:06 -0300

    transferindo

---
+++ helpbar/src/Libras.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Libras = () =>{
+    return(
+        <div>
+            <h1>Libras</h1>
+            <Link to="/">retornar a pagina inicial</Link>
+        </div>
+    );
+}
+
+export default Libras;
---
+++ helpbar/src/Voz.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Voz = () =>{
+    return(
+        <div>
+            <h1>Voz</h1>
+            <Link to="/">retornar a pagina inicial</Link>
+        </div>
+    );
+}
+
+export default Voz;
